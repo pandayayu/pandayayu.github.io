2988ce722e5159a9feb34a66ccc97431d724ff15
pandayayu <156371589+pandayayu@users.noreply.github.com>
2024-03-28 23:14:45 -0400

    done

--- Assignment-4/Part-4/main-finished.js
+++ Assignment-4/Part-4/main-finished.js
@@ -90,9 +90,12 @@ class Ball extends Shape {
 class EvilCircle extends Shape {
 
     constructor(x,y) {
+        super(x, y, 20,20);
+
+        
         this.color = "white";
         this.size = 10;
-        super(x, y, 20,20);
+        
 
         window.addEventListener('keydown', (e) => {
             switch(e.key) {
