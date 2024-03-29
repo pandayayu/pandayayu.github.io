d7ed01a3f3a08ee96db93492a36d0a3997afebb7
pandayayu <156371589+pandayayu@users.noreply.github.com>
2024-03-28 23:07:07 -0400

    some error needs to be resolved

--- Assignment-4/Part-4/main-finished.js
+++ Assignment-4/Part-4/main-finished.js
@@ -1,4 +1,6 @@
 // set up canvas
+const para = document.querySelector('p');
+let count = 0;
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,14 +8,17 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-const para = document.querySelector('p');
-let count = 0;
 
 // function to generate random number
+function random(min, max){
+    const num = Math.floor(Math.random()* (max- min)) + min;
+    return num;
+}
 
+/*
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+}*/
 
 // function to generate random RGB color value
 
@@ -36,6 +41,7 @@ class Ball extends Shape {
     super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -187,6 +193,10 @@ function loop() {
     ball.update();
     ball.collisionDetect();}
   }
+
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
 }
